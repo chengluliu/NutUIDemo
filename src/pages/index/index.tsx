d946b8e059a082113e5be2d162c9bb57abb6a126
风影 <15766799@qq.com>
2022-11-24 10:57:42 +0800

    更新Dialog问题

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 import { useDidShow } from "@tarojs/taro";
-import { Button, Cell } from "@nutui/nutui-react-taro";
+import { Button, Dialog } from "@nutui/nutui-react-taro";
 
 const Index = () => {
+  const [visible, setVisible] = useState(true);
   return (
     <>
-      <Button shape="square" plain type="primary" icon="star-fill" />
-
-      <Cell title="我的追剧" replace={true} to="/pages/cell/index" />
+      <Dialog
+        title="组件调用"
+        visible={visible}
+        onOk={() => setVisible(false)}
+        onCancel={() => setVisible(false)}
+      >
+        如果需要在弹窗内嵌入组件或其他自定义内容，可以使用组件调用的方式。
+      </Dialog>
     </>
   );
 };
