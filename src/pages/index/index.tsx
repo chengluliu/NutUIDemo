98125ecada55c7c288e4a00b3f9dea580faa1064
风影 <15766799@qq.com>
2023-01-10 19:44:14 +0800

    提交issue

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,20 +1,14 @@
 import React, { useState } from "react";
 import { useDidShow } from "@tarojs/taro";
-import { Button, Dialog } from "@nutui/nutui-react-taro";
-
+import { Badge, Avatar } from "@nutui/nutui-react-taro";
+import "./index.scss";
 const Index = () => {
-  const [visible, setVisible] = useState(true);
   return (
-    <>
-      <Dialog
-        title="组件调用"
-        visible={visible}
-        onOk={() => setVisible(false)}
-        onCancel={() => setVisible(false)}
-      >
-        如果需要在弹窗内嵌入组件或其他自定义内容，可以使用组件调用的方式。
-      </Dialog>
-    </>
+    <div className="main">
+      <Badge value={8} color="#3E8AA0">
+        <Avatar icon="my" shape="square" />
+      </Badge>
+    </div>
   );
 };
 export default Index;
